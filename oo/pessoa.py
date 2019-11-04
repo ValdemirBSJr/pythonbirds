051296ae440721329306e55f27a7181761b70ede
valdemir <badmoon25@gmail.com>
2019-11-04 08:15:14 -0300

    criado atributos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+
+    olhos = 2 #Atributo de classe/default -> Comum e igual a todos os objetos. Economiza memoria
     
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
@@ -29,3 +31,16 @@ if __name__=='__main__':
     luciano.sobrenome = 'Ramalho' #adiciona atributo dinamicamente
     print(luciano.__dict__)
     print(renzo.__dict__) #ve qual atributos tem
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos)) #São o mesmo elemento
+
+    luciano.olhos = 1
+
+    print(luciano.__dict__) #se mudar o valor, ele passa a pertebcer ao grupo de atributo
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos)) #O de luciaono agora é diferente
+
+    Pessoa.olhos = 3 #Agora todo mundo terá 3 olhos, menos luciano puq foi explicitado que ele tem 1
+
+    print(Pessoa.olhos)
+    print(luciano.olhos)
